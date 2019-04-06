63698c4292f1335a3a6343d36f061a4706b96ac8
Joemar Brillo <joemar.brillo@outlook.com>
2019-04-06 09:54:54 +0800

    Initial commits for Xcode project.

---
+++ IBM iBank/IBM iBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBM iBank
+//
+//  Created by Joemar Brillo on 6/4/19.
+//  Copyright © 2019 Joemar Brillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IBM iBank/IBM iBankTests/IBM_iBankTests.swift
@@ -0,0 +1,34 @@
+//
+//  IBM_iBankTests.swift
+//  IBM iBankTests
+//
+//  Created by Joemar Brillo on 6/4/19.
+//  Copyright © 2019 Joemar Brillo. All rights reserved.
+//
+
+import XCTest
+@testable import IBM_iBank
+
+class IBM_iBankTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
