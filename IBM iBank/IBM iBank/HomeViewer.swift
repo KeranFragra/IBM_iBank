e6d61d0f5eae2e4e605bd477f4af9aad39144508
Joemar Brillo <joemar.brillo@outlook.com>
2019-04-06 10:01:43 +0800

    Added main/home screen.

---
+++ IBM iBank/IBM iBank/HomeViewer.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewer.swift
+//  IBM iBank
+//
+//  Created by Joemar Brillo on 6/4/19.
+//  Copyright © 2019 Joemar Brillo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// The main/home screen for the app.
+class HomeViewer: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        title = "IBM iBank"
+    }
+}
